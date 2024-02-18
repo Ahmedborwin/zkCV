a7299794fcf24e423a41f3bb311391cb79234a96
proconectach38 <contact@proconecta.com>
2024-02-18 15:20:21 +0200

    FE - redesign hook

--- src/hooks/useVerifiedProofs.js
+++ src/hooks/useVerifiedProofs.js
@@ -12,25 +12,39 @@ import { SemaphoreEthers } from "@semaphore-protocol/data";
 
 export const useVerifiedProofs = (groupId) => {
     const [verifiedProofs, setVerifiedProofs] = useState(null);
+    const [semaphoreEthers, setSemaphoreEthers] = useState(null);
     const chainId = useSelector(selectChainId);
 
     useEffect(() => {
-        const fetchVerifiedProofs = async () => {
+        const fetchSemaphoreEthers = () => {
             try {
-                const semaphoreEthers = new SemaphoreEthers(
+                const newSemaphoreEthers = new SemaphoreEthers(
                     "https://polygon-mumbai.g.alchemy.com/v2/zTPogX-iVpVC1-IGvBRCJYI6hX6DLNKP",
                     {
                         address: SemaphoreAddressFile[chainId],
                         startBlock: 0,
                     });
+                setSemaphoreEthers(newSemaphoreEthers);
+            }
+            catch (error) {
+                console.error("Failed to fetch semaphore ethers:", error);
+            }
+        }
+
+        if (chainId) fetchSemaphoreEthers();
+    }, [chainId])
+
+    useEffect(() => {
+        const fetchVerifiedProofs = async () => {
+            try {
                 const proofs = await semaphoreEthers.getGroupVerifiedProofs(groupId.toString());
                 setVerifiedProofs(proofs);
             } catch (error) {
                 console.error("Failed to fetch verified proofs:", error);
             }
         };
-        if (groupId && chainId) fetchVerifiedProofs();
-    }, [groupId, chainId]);
+        if (groupId && semaphoreEthers) fetchVerifiedProofs();
+    }, [groupId, semaphoreEthers]);
 
     return verifiedProofs;
 };
